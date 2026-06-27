2db94c5eff4741055ba6040991137bba6a7d10d6
Victor <contact@norsecoder.com>
2026-06-27 21:41:43 +0200

    Suppression de l'icône Mic inutilisée dans le composant Icons

--- frontend/components/ui/Icons.tsx
+++ frontend/components/ui/Icons.tsx
@@ -276,15 +276,6 @@ export const VideoOff = createIcon(({ Path, Line }) => (
   </>
 ));
 
-export const Mic = createIcon(({ Path, Line }) => (
-  <>
-    <Path d="M12 1a3 3 0 00-3 3v8a3 3 0 006 0V4a3 3 0 00-3-3z" />
-    <Path d="M19 10v2a7 7 0 01-14 0v-2" />
-    <Line x1="12" y1="19" x2="12" y2="23" />
-    <Line x1="8" y1="23" x2="16" y2="23" />
-  </>
-));
-
 export const MicOff = createIcon(({ Path, Line }) => (
   <>
     <Line x1="1" y1="1" x2="23" y2="23" />
